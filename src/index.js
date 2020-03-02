1c22f62947ae2e7f7913731ea4273d5bf7c68c90
Phong Ngo <phongngo91@gmail.com>
2020-03-02 10:50:52 -0500

    Built content container

--- src/index.js
+++ src/index.js
@@ -7,18 +7,21 @@ window.addEventListener("DOMContentLoaded", () => {
   const contentContainer = document.createElement("div");
   contentContainer.classList.add("content-container");
 
-  const leftNav = document.createElement("div");
-  leftNav.classList.add("left-nav");
-
   const topNav = document.createElement("div");
   topNav.classList.add("top-nav");
 
+  const leftNav = document.createElement("div");
+  leftNav.classList.add("left-nav");
   const gameContainer = document.createElement("div");
   gameContainer.classList.add("game-container");
 
-  contentContainer.appendChild(gameContainer);
-  contentContainer.appendChild(leftNav);
+  const bottomSection = document.createElement("div");
+  bottomSection.classList.add("bottom-section");
+  bottomSection.appendChild(leftNav);
+  bottomSection.appendChild(gameContainer);
+
   contentContainer.appendChild(topNav);
+  contentContainer.appendChild(bottomSection);
 
   document.body.appendChild(contentContainer);
 });
