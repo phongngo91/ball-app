118a392279d7cb14a46e72818929aabe9ab7c7c8
Phong Ngo <phongngo91@gmail.com>
2020-03-02 13:48:53 -0500

    Create first level

--- src/js/ball.js
+++ src/js/ball.js
@@ -1,7 +1,11 @@
 import * as THREE from "./three";
 
-var geometryBall = new THREE.SphereGeometry(1, 5, 5);
-var materialBall = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+var texture = new THREE.TextureLoader().load("src/images/tree_bg.jpg");
+
+
+var geometryBall = new THREE.SphereGeometry(1, 32, 32);
+// var materialBall = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+var materialBall = new THREE.MeshBasicMaterial({ map: texture });
 var ball = new THREE.Mesh(geometryBall, materialBall);
 
 export default ball;
--- src/js/game.js
+++ src/js/game.js
@@ -4,7 +4,6 @@ import level_1 from "./level_1";
 
 const gameContainer = document.createElement("div");
 gameContainer.classList.add("game-container");
-
 let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(
   75,
@@ -14,51 +13,73 @@ let camera = new THREE.PerspectiveCamera(
 );
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth * (6/10), window.innerHeight * (6/10));
+renderer.setSize(window.innerWidth * (6 / 10), window.innerHeight * (6 / 10));
 
-gameContainer.appendChild(renderer.domElement);
+function restart() {
 
-scene.add(ball);
-scene.add(level_1);
+  const oldGameOver = document.getElementsByClassName('game-over-text');
 
-camera.position.z = 5;
-
-const moveSpeed = 0.5;
-
-const LEFT_ARROW = 37;
-const UP_ARROW = 38;
-const RIGHT_ARROW = 39;
-const DOWN_ARROW = 40;
-
-document.addEventListener("keydown", e => {
-  var keyCode = e.which;
-  if (keyCode == UP_ARROW) {
-    ball.position.y += moveSpeed;
-  } else if (keyCode == DOWN_ARROW) {
-    ball.position.y -= moveSpeed;
-  } else if (keyCode == LEFT_ARROW) {
-    ball.position.x -= moveSpeed;
-  } else if (keyCode == RIGHT_ARROW) {
-    ball.position.x += moveSpeed;
-  }
-  renderer.render(scene, camera);
-});
-
-var animate = function() {
-  requestAnimationFrame(animate);
-  ball.rotation.x -= 0.01;
-  ball.rotation.y -= 0.01;
-
-  // Move the level down
-  level_1.position.y -= 0.1;
-  console.log(level_1.position.y);
-
-  if (level_1.position.y < -14){
-    level_1.position.y = 14;
+  if (oldGameOver.length > 0){
+    gameContainer.removeChild(oldGameOver[0]);
   }
 
-  renderer.render(scene, camera);
-};
-animate();
+  gameContainer.appendChild(renderer.domElement);
+
+  scene.add(ball);
+  scene.add(level_1);
+  level_1.position.y = 10;
+
+  camera.position.z = 5;
+
+  const moveSpeed = 0.5;
+
+  const LEFT_ARROW = 37;
+  const UP_ARROW = 38;
+  const RIGHT_ARROW = 39;
+  const DOWN_ARROW = 40;
+  const R_KEY = 82;
+
+  document.addEventListener("keydown", e => {
+    var keyCode = e.which;
+    if (keyCode == DOWN_ARROW) {
+      level_1.position.y += moveSpeed;
+      ball.rotation.x += moveSpeed;
+    } else if (keyCode == UP_ARROW) {
+      level_1.position.y -= moveSpeed;
+      ball.rotation.x -= moveSpeed;
+    } else if (keyCode == RIGHT_ARROW) {
+      level_1.position.x -= moveSpeed;
+      ball.rotation.y -= moveSpeed;
+    } else if (keyCode == LEFT_ARROW) {
+      level_1.position.x += moveSpeed;
+      ball.rotation.y += moveSpeed;
+    } else if (keyCode == R_KEY) {
+      restart();
+    }
+
+    if (level_1.position.y < -10) {
+      var gameWindow = document.querySelector("canvas");
+      if (gameWindow) {
+        gameContainer.removeChild(gameWindow);
+
+        const gameOver = document.createElement("div");
+        gameOver.classList.add("game-over-text");
+        gameOver.innerHTML = "GAME OVER, press r to restart";
+        gameContainer.appendChild(gameOver);
+      }
+    }
+    console.log(level_1.position.y);
+    renderer.render(scene, camera);
+  });
+
+  var animate = function() {
+    requestAnimationFrame(animate);
+
+    renderer.render(scene, camera);
+  };
+  animate();
+}
+
+restart();
 
 export default gameContainer;
