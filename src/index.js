a37544f632246f77d0b37852b7e7fb3b98a8d133
Phong Ngo <phongngo91@gmail.com>
2020-03-02 11:00:40 -0500

    Fit renderer to game container

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./styles/index.scss";
+import * as THREE from "./js/three";
 
 window.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("center");
@@ -12,8 +13,40 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const leftNav = document.createElement("div");
   leftNav.classList.add("left-nav");
+
   const gameContainer = document.createElement("div");
   gameContainer.classList.add("game-container");
+
+  let scene = new THREE.Scene();
+  let camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  );
+
+  var renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+  gameContainer.appendChild(renderer.domElement);
+
+  var geometrySphere = new THREE.SphereGeometry(1, 5, 5);
+  var materialSphere = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+  var sphere = new THREE.Mesh(geometrySphere, materialSphere);
+
+  scene.add(sphere);
+  camera.position.z = 5;
+  camera.position.z = 5;
+
+  var animate = function() {
+    requestAnimationFrame(animate);
+
+    sphere.rotation.x += 0.01;
+    sphere.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+  };
+
+  animate();
 
   const bottomSection = document.createElement("div");
   bottomSection.classList.add("bottom-section");
